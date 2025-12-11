f6b4b237bc91f1b88ffb9c84109e6d5fb35402e0
princeuchennafrancais <princeuchenna733@gmail.com>
2025-12-11 09:26:25 +0100

    Add full project

---
+++ lib/auth_pages/login_page.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class LoginPage extends StatelessWidget {
+  const LoginPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Center(
+        child: Text("this is it", style: TextStyle(
+          color: Colors.black,
+        ),),
+      ),
+    );
+  }
+}
